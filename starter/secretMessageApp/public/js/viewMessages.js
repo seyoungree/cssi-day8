7fbec5c8deff6e8d728524b61e4c09b4d6b8160f
seyoungree <seyoungree@gmail.com>
2021-07-22 14:55:58 +0000

    informative message

---
+++ starter/secretMessageApp/public/js/viewMessages.js
@@ -0,0 +1,33 @@
+const getMessages = () => {
+    const messagesRef = firebase.database().ref();
+    messagesRef.on('value', (snapshot) => {
+        const data = snapshot.val();
+        console.log(data);
+        for (let key in data){
+            const message = data[key];
+            console.log(message);
+            if(myPass == message.passcode){
+                renderMessage(message);
+            }
+        }
+    });
+}
+
+const findMessage = (myPass) => {
+    const messagesRef = firebase.database().ref();
+    messagesRef.on('value', (snapshot) => {
+        const data = snapshot.val();
+        console.log(data);
+        for (let key in data){
+            const message = data[key];
+            console.log(message);
+            if(myPass == message.passcode){
+                renderMessage(message);
+            }
+        }
+    });
+}
+document.querySelector("#viewMsg").addEventListener("click", () => {
+    const passcode = document.querySelector("#passcode").value;
+    findMessage(passcode);
+});
